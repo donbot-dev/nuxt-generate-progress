daea878d5097bc260561effe476b36a9ee6d157a
donbot <git@donbot.dev>
2020-08-11 15:19:15 +0300

    chore(release): first release

---
+++ lib/module.ts
@@ -0,0 +1,23 @@
+import consola from 'consola';
+import { Module } from '@nuxt/types';
+
+export const generateProgressModule: Module = function () {
+  // TODO: remove "any" typings after they appear in @nuxt/types
+  this.nuxt.hook('generate:extendRoutes', (routes: any[]) => {
+    const routesTotal = routes.length;
+    let currentRoute = 0;
+
+    this.nuxt.hook(
+      'generate:routeCreated',
+      ({ route, errors }: { route: string; errors: any[] }) => {
+        if (!errors.length) {
+          consola.success(
+            `(${++currentRoute}/${routesTotal}) Generated ${route}`,
+          );
+        }
+      },
+    );
+  });
+};
+
+export default generateProgressModule;
